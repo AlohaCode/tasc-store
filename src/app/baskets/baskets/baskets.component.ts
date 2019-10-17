c5d928027123e8ec6768012349591fbfcad92720
AlohaCode <bretthooper.maui@gmail.com>
2019-10-16 21:10:17 -1000

    Implemented Receipts component and Baskets component

--- src/app/baskets/baskets/baskets.component.ts
+++ src/app/baskets/baskets/baskets.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Basket } from '../../core/models/basket';
+import { BasketService } from '../basket.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-baskets',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./baskets.component.css']
 })
 export class BasketsComponent implements OnInit {
+  selected: Basket;
+  baskets$: Observable<Basket[]>;
+  loading$: Observable<boolean>;
 
-  constructor() { }
+  constructor( private basketSvc: BasketService ) {
+    this.baskets$ = basketSvc.entities$;
+    this.loading$ = basketSvc.loading$;
+  }
 
   ngOnInit() {
+    this.getBaskets();
+  }
+
+  add(basket: Basket) {
+    this.basketSvc.add(basket);
+  }
+
+  delete(basket: Basket) {
+    this.basketSvc.delete(basket);
+    this.close();
+  }
+
+  getBaskets() {
+    this.basketSvc.getAll();
+    this.close();
+  }
+
+  update(basket: Basket) {
+    this.basketSvc.update(basket);
+  }
+
+  close() {
+    this.selected = null;
+  }
+
+  select(basket: Basket) {
+    this.selected = basket;
   }
 
 }
