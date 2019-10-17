3667acd34364c4e0b8b7be992d7b33de0b51de6d
AlohaCode <bretthooper.maui@gmail.com>
2019-10-16 21:24:06 -1000

    Implemented the remainder of store module. Imported store, effects, storeDevTools

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NgrxDataModule } from 'ngrx-data';
 import { entityConfig } from './entity-metadata';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    NgrxDataModule.forRoot(entityConfig)
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    NgrxDataModule.forRoot(entityConfig),
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ]
 })
 export class AppStoreModule { }
