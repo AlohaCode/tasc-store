f4d4ca22b9c10c896f620d60f0ff8bae4e167711
AlohaCode <bretthooper.maui@gmail.com>
2019-10-17 12:11:46 -1000

    updated baskets.module with routing and Material module

--- src/app/baskets/baskets.module.ts
+++ src/app/baskets/baskets.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasketsComponent } from './baskets/baskets.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+import { BasketService } from './basket.service';
 
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: BasketsComponent }
+];
 
 @NgModule({
-  declarations: [BasketsComponent],
+  declarations: [
+    BasketsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [
+    BasketService
   ]
 })
 export class BasketsModule { }
