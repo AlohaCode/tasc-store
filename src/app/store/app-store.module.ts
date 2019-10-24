5c83edfcee94241fc5afbcc8f7eccff7930d8609
AlohaCode <bretthooper.maui@gmail.com>
2019-10-23 16:48:34 -1000

    defined defaultDataService Config, added provider

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { NgrxDataModule } from 'ngrx-data';
+import { NgrxDataModule, DefaultDataServiceConfig } from 'ngrx-data';
 import { entityConfig } from './entity-metadata';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
+
+const defaultDataServiceConfig: DefaultDataServiceConfig = {
+  root: 'http://localhost:9000/api',
+  timeout: 10000, // request timeout
+};
 
 @NgModule({
   declarations: [],
@@ -13,6 +18,12 @@ import { environment } from '../../environments/environment';
     EffectsModule.forRoot([]),
     NgrxDataModule.forRoot(entityConfig),
     environment.production ? [] : StoreDevtoolsModule.instrument()
+  ],
+  providers: [
+    {
+      provide: DefaultDataServiceConfig,
+      useValue: defaultDataServiceConfig
+    }
   ]
 })
 export class AppStoreModule { }
