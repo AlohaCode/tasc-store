8f67977d55f4669c91862fce6de8458991175203
AlohaCode <bretthooper.maui@gmail.com>
2019-10-24 23:54:08 -1000

    implemented baskets resolver

---
+++ src/app/baskets/baskets.resolver.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { tap, first, filter } from 'rxjs/operators';
+import { BasketsService } from './baskets.service';
+
+@Injectable()
+export class BasketsResolver implements Resolve<boolean> {
+
+  constructor(private basketsSvc: BasketsService) { }
+
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<boolean> {
+    return this.basketsSvc.loaded$
+      .pipe(
+        tap(loaded => {
+          if (!loaded) {
+            console.log('getting baskets from db');
+            this.basketsSvc.getAll();
+          }
+        }),
+        filter(loaded => !!loaded),
+        first()
+      );
+  }
+}
