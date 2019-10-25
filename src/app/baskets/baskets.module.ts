d8c2b00920a576369275e3e74310f0284703c073
AlohaCode <bretthooper.maui@gmail.com>
2019-10-24 23:56:29 -1000

    added new parameterized path for receipts component navigation, added resolver to path configs

--- src/app/baskets/baskets.module.ts
+++ src/app/baskets/baskets.module.ts
@@ -3,15 +3,34 @@ import { CommonModule } from '@angular/common';
 import { BasketsComponent } from './baskets/baskets.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
-import { BasketService } from './basket.service';
+import { BasketsService } from './baskets.service';
+import { BasketsResolver } from './baskets.resolver';
+import { ReceiptsService } from '../receipts/receipts.service';
+import { ReceiptsComponent } from '../receipts/reciepts/receipts.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: BasketsComponent }
+  {
+    path: '',
+    pathMatch: 'full',
+    component: BasketsComponent,
+    resolve: {
+      baskets: BasketsResolver
+    }
+  },
+  {
+    path: ':id',
+    pathMatch: 'full',
+    component: ReceiptsComponent,
+    resolve: {
+      baskets: BasketsResolver
+    }
+  }
 ];
 
 @NgModule({
   declarations: [
-    BasketsComponent
+    BasketsComponent,
+    ReceiptsComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +38,9 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
   providers: [
-    BasketService
+    BasketsService,
+    ReceiptsService,
+    BasketsResolver
   ]
 })
 export class BasketsModule { }
