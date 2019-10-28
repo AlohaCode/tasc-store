8fd57752fdff124987c32abaea82d190bb8d2151
AlohaCode <bretthooper.maui@gmail.com>
2019-10-27 18:44:52 -1000

    cleaned up tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { RouterModule, ChildrenOutletContexts } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +9,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        CoreModule,
+        RouterModule
+      ],
+      providers: [
+        ChildrenOutletContexts
+      ]
     }).compileComponents();
   }));
 
@@ -14,18 +23,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tascTask'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tascTask');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tascTask app is running!');
   });
 });
